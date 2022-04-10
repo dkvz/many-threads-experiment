2ab35a0c1dc81d83779a32ba9e7750f18c970a17
dkvz <github@dkvz.eu>
2022-04-10 15:55:49 +0200

    Add sleeper thread test

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,47 @@ use std::env;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::{self, JoinHandle};
+use std::time;
+
+// Basically the test parameters are here:
+const THREAD_COUNT: i32 = 1500;
+const SLEEP_INTERVAL: time::Duration = time::Duration::from_secs(1);
 
 fn yielder_thread(id: &i32, condition: Arc<AtomicBool>) {
-  println!("Thread {} started.", id);
+  println!("Yielder thread {} started.", id);
   while condition.load(Ordering::SeqCst) == false {
     thread::yield_now();
   }
 }
 
+// This uses the same amount of CPU as with the
+// atomic check.
+fn yielder_thread_no_atomic(id: &i32) {
+  println!("Yielder (no atomic check) thread {} started.", id);
+  loop {
+    thread::yield_now();
+  }
+}
+
+fn sleeper_thread(id: &i32, condition: Arc<AtomicBool>) {
+  println!("Sleeper thread {} started.", id);
+  while condition.load(Ordering::SeqCst) == false {
+    thread::sleep(SLEEP_INTERVAL);
+  }
+}
+
 fn main() {
-  let thread_count = 24;
   let dumb_condition = Arc::new(AtomicBool::new(false));
   let args: Vec<String> = env::args().collect();
   let mode: &str = if args.len() >= 2 { &args[1] } else { "0" };
   let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
-  for i in 1..thread_count {
+  for i in 1..THREAD_COUNT {
     let condition_clone = dumb_condition.clone();
     match mode {
       "1" => handles.push(thread::spawn(move || yielder_thread(&i, condition_clone))),
+      "2" => handles.push(thread::spawn(move || yielder_thread_no_atomic(&i))),
+      "3" => handles.push(thread::spawn(move || sleeper_thread(&i, condition_clone))),
       _ => println!("Please provide mode as argument, 1 to something"),
     }
   }
