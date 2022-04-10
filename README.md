90483d85f02fb302419e3ff81c7eff57a0dc7aac
dkvz <github@dkvz.eu>
2022-04-10 15:32:16 +0200

    Prepare for more tests

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Experiment ideas
 - A lot of sleeping threads that are set with a long duration;
 - Sleeping threads with lower duration, that immediately go back to sleep;
-- Threads that call "yield_now" immediately if a condition isn't met -> They didn't lie in the doc this uses a lot of CPU;
+- Threads that call "yield_now" immediately if a condition isn't met -> They didn't lie in the doc this uses a lot of CPU which makes sense since there's no blocking at all;
 - Threads waiting for a [Condvar](https://doc.rust-lang.org/stable/std/sync/struct.Condvar.html) - This should have the least impact.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::{self, JoinHandle};
@@ -10,16 +11,19 @@ fn yielder_thread(id: &i32, condition: Arc<AtomicBool>) {
 }
 
 fn main() {
+  let thread_count = 24;
   let dumb_condition = Arc::new(AtomicBool::new(false));
-
+  let args: Vec<String> = env::args().collect();
+  let mode: &str = if args.len() >= 2 { &args[1] } else { "0" };
   let mut handles: Vec<JoinHandle<()>> = Vec::new();
 
-  for i in 1..10 {
+  for i in 1..thread_count {
     let condition_clone = dumb_condition.clone();
-    handles.push(thread::spawn(move || yielder_thread(&i, condition_clone)));
+    match mode {
+      "1" => handles.push(thread::spawn(move || yielder_thread(&i, condition_clone))),
+      _ => println!("Please provide mode as argument, 1 to something"),
+    }
   }
-
-  println!("Test started.");
 
   for h in handles {
     h.join().unwrap();
